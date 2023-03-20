10499d4b259d653839c23fc489b64a055a46ff2c
Alma Cleto <emscleto11@gmail.com>
2023-03-21 01:00:56 +0800

    updated navbar

--- project.js
+++ project.js
@@ -1,7 +1,7 @@
 var nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function() {
-    if(this.window.pageYOffset > 200) {
+    if(this.window.pageYOffset > 100) {
         nav.classList.add('bg-black', 'shadow');
     } else {
         nav.classList.remove('bg-black', 'shadow');
